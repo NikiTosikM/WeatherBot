744ef99ee7fedee93fac39a6f2a03695e3345da9
Nikita Marykov <marykov63@mail.ru>
2023-09-28 09:06:41 +0400

    bot work with voice messages

--- GetWeather.py
+++ GetWeather.py
@@ -1,11 +1,14 @@
 from main_dates import API_WEATHER
 from pprint import pprint
 import requests
+import whisper
+import os
+from aiogram import types
 from datetime import datetime
 from aiogram import types
 
 
-def get_weather(city, api):
+async def get_weather(city, api):
 
     try:
         weather_dates = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}&lang=ru&units=metric')
@@ -27,3 +30,29 @@ def get_weather(city, api):
 def request_weather(city):
 
     return get_weather(city, API_WEATHER)
+
+async def get_city_from_voice(message: types.Message):
+
+    from WeatherBot import bot
+
+    file_id = message.voice.file_id
+    file = await bot.get_file(file_id)
+    file_path = file.file_path
+    await bot.download_file(file_path, 'voice/user_voice.wav')
+
+    model = whisper.load_model("small")
+    voice_user_path = r'voice\user_voice.wav'
+    city_voice = model.transcribe(voice_user_path)
+
+    symbols = {',': None, '.': None, ' ': None, ';': None}
+    translate_symbols = str.maketrans(symbols)
+
+    result_city = city_voice['text'].translate(translate_symbols)
+
+    os.remove(r'voice\user_voice.wav')
+
+    return result_city
+
+
+
+
--- WeatherBot.py
+++ WeatherBot.py
@@ -4,6 +4,8 @@ import tracemalloc
 from main_dates import TOKEN_BOT , API_WEATHER
 from GetWeather import get_weather
 from Keyboard import keyboard_buttons
+from GetWeather import get_city_from_voice, get_weather
+
 
 
 bot = Bot(token=TOKEN_BOT) 
@@ -15,11 +17,26 @@ async def start_bot (message: types.Message):
 
 @dp.message_handler()
 async def weather_city(message: types.Message):
-    dates, check = get_weather(message.text, API_WEATHER)
+    dates, check = await get_weather(message.text, API_WEATHER)
 
     keyboard = keyboard_buttons(message.text, check)
 
     await message.answer(dates, reply_markup= keyboard )
+
+@dp.message_handler(content_types=types.ContentType.VOICE)
+async def weather_voice(message: types.Message):
+
+    await message.answer('Подождите... идет распознавание текста ')
+
+    gif_image = open('images\_s_tarelkami_gif_yapfiles.ru.gif', 'rb')
+
+    await bot.send_animation(chat_id=message.from_user.id, animation=gif_image)
+
+    city_voice = await get_city_from_voice(message)
+
+    city_weather, check = await get_weather(city_voice, API_WEATHER)
+
+    await message.answer(city_weather)
 
 if __name__ == '__main__':
     tracemalloc.start()
